bcdb7a111859f1fd684db0b7022be1cc7709c46d
1winhyun <jimking1@naver.com>
2025-09-08 15:03:05 +0900

    시작

---
+++ src/main/java/com/exercise/exercise/ExerciseController.java
@@ -0,0 +1,13 @@
+package com.exercise.exercise;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExerciseController {
+	@GetMapping("/exercise")
+	public String exercise(){
+		System.out.println("exercise controller");
+		return "hello world";
+	}
+}
